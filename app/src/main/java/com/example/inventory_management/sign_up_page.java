d7b81c0558af34261054986fb1780215da27de1c
goutambothra1 <jaingautam28@gmail.com>
2023-10-15 00:12:57 +0530

    signup email, password regex validations debugging database operations

--- app/src/main/java/com/example/inventory_management/sign_up_page.java
+++ app/src/main/java/com/example/inventory_management/sign_up_page.java
@@ -1,6 +1,7 @@
 package com.example.inventory_management;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,7 +29,12 @@ public class sign_up_page extends AppCompatActivity {
         password=findViewById(R.id.first_password);
         confirm_password   =    findViewById(R.id.cnfrm_password);
         SignUpButton=(Button) findViewById(R.id.signup_btn);
-        dbhelper=new DBHelper(this);
+        dbhelper= new DBHelper(this) {
+            @Override
+            public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+            }
+        };
         backsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,44 +42,52 @@ public class sign_up_page extends AppCompatActivity {
                 startActivity(back);
             }
         });
-        SignUpButton.setOnClickListener(new View.OnClickListener(){
-
+        SignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String branch,dept,emails,passwords,confirm_passwords;
-                branch=branch_name.getText().toString();
-                dept=dept_name.getText().toString();
-                emails=email.getText().toString();
-                passwords=password.getText().toString();
-                confirm_passwords=confirm_password.getText().toString();
+                String branch, dept, emails, passwords, confirm_passwords;
+                branch = branch_name.getText().toString();
+                dept = dept_name.getText().toString();
+                emails = email.getText().toString();
+                passwords = password.getText().toString();
+                confirm_passwords = confirm_password.getText().toString();
 
-                if(branch.equals("")||dept.equals("")||emails.equals("")||passwords.equals("")||confirm_passwords.equals(""))
-                {
-                    Toast.makeText(getApplicationContext(),"input is required",Toast.LENGTH_SHORT).show();
-                }
-                else {
-                   if(passwords.equals(confirm_passwords))
-                   {
-                       if(dbhelper.checkusers(branch))
-                       {
-                           Toast.makeText(getApplicationContext(),"Branch Already Exist",Toast.LENGTH_SHORT).show();
-                            return;
+                // Email validation regex
+                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
-                       }
-                       //input data
-                       boolean result_insert=dbhelper.insertdata(branch, dept,emails, passwords,confirm_passwords);
-                       if(result_insert==false)
-                       {
-                           Toast.makeText(getApplicationContext(),"error while Insert data",Toast.LENGTH_SHORT).show();
-                       }
-                       else {
-                           Toast.makeText(getApplicationContext(),"data Insert",Toast.LENGTH_SHORT).show();
-                       }
-                   }
-                   else {
-                       Toast.makeText(getApplicationContext(),"Password do not match",Toast.LENGTH_SHORT).show();
-                   }
+                // Password validation regex
+                String passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{6,}$";
 
+                if (branch.equals("") || dept.equals("") || emails.equals("") || passwords.equals("") || confirm_passwords.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Input is required", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (!emails.matches(emailPattern)) {
+                        Toast.makeText(getApplicationContext(), "Invalid email address", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    if (!passwords.matches(passwordPattern)) {
+                        Toast.makeText(getApplicationContext(), "Invalid password format", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    if (!passwords.equals(confirm_passwords)) {
+                        Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    if (dbhelper.checkusers(branch)) {
+                        Toast.makeText(getApplicationContext(), "Branch Already Exists", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    // Input data
+                    boolean result_insert = dbhelper.insertdata(branch, dept, emails, passwords, confirm_passwords);
+                    if (!result_insert) {
+                        Toast.makeText(getApplicationContext(), "Error while inserting data", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
